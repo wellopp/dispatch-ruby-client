5c8347b437e5c10d772dd95bd90c686f8df539aa
Jacob Evan Shreve <jacobevanshreve@me.com>
2017-09-07 17:32:43 -0400

    Add a lot of validation to simplify email and sms helpers

--- lib/dispatch.rb
+++ lib/dispatch.rb
@@ -5,68 +5,132 @@ require_relative 'dispatch/delivery'
 require_relative 'dispatch/errors'
 require_relative 'dispatch/version'
 
+#
+# Dispatch Client
+#
+# Dispatch messages where they need to be
+#
 module Dispatch
+  SMS_PARAMS = %i(to body test).freeze
+
+  EMAIL_PARAMS = %i(
+    html
+    text
+    body
+    subject
+    from
+    from_name
+    from_email
+    to
+    headers
+    important
+    track_clicks
+    track_opens
+    auto_text
+    auto_html
+    inline_css
+    url_strip_qs
+    preserve_recipients
+    view_content_link
+    bcc_address
+    tracking_domain
+    signing_domain
+    return_path_domain
+    merge
+    merge_language
+    global_merge_vars
+    merge_vars
+    tags
+    subaccount
+    google_analytics_domains
+    google_analytics_campaign
+    metadata
+    recipient_metadata
+    attachments
+    images
+  ).freeze
+
   @config = Config.new
 
-  def self.config
-    yield(@config) if block_given?
-    @config
-  end
+  class << self
+    def config
+      yield(@config) if block_given?
+      @config
+    end
 
-  def self.status
-    HTTParty.get(@config[:endpoint]).parsed_response['status']
-  end
+    def status
+      HTTParty.get(@config[:endpoint]).parsed_response['status']
+    end
 
-  def self.find(guid)
-    Delivery.parse HTTParty.get("#{@config[:endpoint]}/deliveries/#{guid}.json")
-  end
+    def find(guid)
+      Delivery.parse HTTParty.get("#{@config[:endpoint]}/deliveries/#{guid}.json")
+    end
 
-  def self.sms(to:, body:, test: false)
-    raise EmptyArgumentError.new(:to, to) if to.nil? || to.empty?
-    raise EmptyArgumentError.new(:body, body) if body.nil? || body.empty?
+    def sms(params)
+      validate_params(SMS_PARAMS, params)
+      validate_required(params, :to, :body)
+      validate_matches(/[^+0-9\(\)\s]/, 'phone number', to)
 
-    if to.is_a?(Array)
-      to.each do |t|
-        raise InvalidArgumentError.new(:"phone number", t) if t.match?(/[^+0-9\(\)\s]/)
+      deliver(params.merge(tech: 'sms'))
+    end
+
+    def email(params)
+      validate_params(EMAIL_PARAMS, params)
+      validate_required(params, :to,
+                        body: -> { params[:attachments].nil? || params[:attachments].empty? })
+
+      deliver(params.merge(tech: 'email'))
+    end
+
+    def deliver(options)
+      app = @config[:app]
+      endpoint = @config[:endpoint]
+
+      raise EmptyArgumentError.new(:App, app) if app.nil? || app.empty?
+      raise EmptyArgumentError.new(:Endpoint, endpoint) if endpoint.nil? || endpoint.empty?
+
+      params = { delivery: options.compact.merge(app: app) }
+
+      response = HTTParty.post("#{endpoint}/deliveries.json",
+                               body: params.to_json,
+                               headers: { 'Content-Type' => 'application/json' })
+
+      Delivery.parse(response)
+    end
+
+    private
+
+    def validate_params(source, params)
+      params.each_pair do |key, _value|
+        next if source.include?(key)
+        raise ::ArgumentError, "unknown parameter #{key}"
       end
-    elsif to.match?(/[^+0-9\(\)\s]/)
-      raise InvalidArgumentError.new(:"phone number", to)
     end
 
-    deliver(to: { phone_number: to }, body: body, test: test)
-  end
-
-  def self.email(to:, from: nil, body: nil, attachments: nil, data: nil, tags: nil)
-    raise EmptyArgumentError.new(:to, to) if to.nil? || to.empty?
-    if (body.nil? || body.empty?) && (attachments.nil? || attachments.empty?)
-      raise EmptyArgumentError.new(:body, body)
+    # validate that the provided required keys are included
+    # if a lambda is provided, the key will only be required if the lambda
+    # evaluates as true
+    def validate_required(params, *keys)
+      keys.each do |key|
+        if key.is_a?(Hash)
+          key.each_pair do |nkey, proc|
+            value = params[nkey]
+            invalid = (value.nil? || value.empty?) && proc.call
+            raise EmptyArgumentError.new(nkey, value) if invalid
+          end
+        else
+          value = params[key]
+          invalid = value.nil? || value.empty?
+          raise EmptyArgumentError.new(key, value) if invalid
+        end
+      end
     end
 
-    if (data.nil? || data.empty?) && !(body.nil? || body.empty?)
-      raise EmptyArgumentError.new(:data, data)
+    def validate_matches(pattern, name, *values)
+      values.flatten.each do |value|
+        next if value.match?(pattern)
+        raise InvalidArgumentError.new(name, value)
+      end
     end
-
-    deliver(to: to, from: from, body: body, attachments: attachments, data: data, tags: tags)
   end
-
-  def self.deliver(options)
-    app = @config[:app]
-    endpoint = @config[:endpoint]
-
-    raise EmptyArgumentError.new(:App, app) if app.nil? || app.empty?
-    raise EmptyArgumentError.new(:Endpoint, endpoint) if endpoint.nil? || endpoint.empty?
-
-    params = { delivery: options.compact.merge(app: app) }
-
-    response = HTTParty.post("#{endpoint}/deliveries.json",
-                             body: params.to_json,
-                             headers: { 'Content-Type' => 'application/json' })
-
-    Delivery.parse(response)
-  end
-end
-
-Dispatch.config do |c|
-  c.app = :messenger_qa
-  c.endpoint = 'http://localhost:3000'
 end
